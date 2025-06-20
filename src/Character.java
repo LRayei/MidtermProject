f6e3dfee04dbcf6ffa07cf4ec95b986d7062003f
246097 <246097@std.hi-joho.ac.jp>
2025-06-20 12:28:42 +0900

    更新

---
+++ src/Character.java
@@ -0,0 +1,15 @@
+public abstract class Character implements Creature {
+    String name;
+    int hp;
+    public abstract void attack(Character target);
+    public final boolean isAlive() {
+        if (hp>0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public void showStatus() {
+        System.out.println(name +   ": HP " + hp);
+    }
+}
---
+++ src/Creature.java
@@ -0,0 +1,4 @@
+public interface Creature {
+    public abstract void attack(Character target);
+
+}
