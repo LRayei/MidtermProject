b147ee8103e8281aa01dd43ec6bf392ffd8d2756
246097 <246097@std.hi-joho.ac.jp>
2025-06-20 12:28:42 +0900

    更新

--- src/Character.java
+++ src/Character.java
@@ -1,15 +1,18 @@
 public abstract class Character implements Creature {
     String name;
     int hp;
-    public abstract void attack(Character target);
+    public Character(String name, int hp){
+        this.name = name;
+        this.hp = hp;
+    }
     public final boolean isAlive() {
-        if (hp>0){
+        if(hp>0){
             return true;
         } else {
             return false;
         }
     }
     public void showStatus() {
-        System.out.println(name +   ": HP " + hp);
+        System.out.println(name +   ":HP " + hp);
     }
 }
--- src/Hero.java
+++ src/Hero.java
@@ -1,4 +1,21 @@
 public class Hero extends Character {
     String weapon;
+    public Hero (String name,int hp,String weapon){
+        super(name,hp);
+        this.name = name;
+        this.hp = hp;
+        this.weapon =weapon;
+    }
+    public void attack(Character target){
+        System.out.println(name + "は"+weapon+"で攻撃!"+target.name+"に10のダメージを与えた!");
+        target.hp -= 10;
+
+    }
+    public void heal(){
+        System.out.println(name+"は回復呪文を唱えた!HPが２０回復した!");
+       hp += 20;
+    }
+
+
 
 }
