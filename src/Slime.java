3b42eadf4fa5b7e00084a838bd7861e4e6a39834
black <246097@std.hi-joho.ac.jp>
2025-06-25 09:51:45 +0900

    ikeda

--- src/Slime.java
+++ src/Slime.java
@@ -1,3 +1,13 @@
-public class Slime extends Character{
+public final class Slime extends Character{
+    public Slime(String name,int hp){
+        super(name,hp);
+        this.name = name;
+        this.hp = hp;
+
+    }
+    public void attack(Character target){
+        System.out.println(name+"は体当たり攻撃!"+target.name+"に５のダメージを与えた!");
+        target.hp -=5;
+    }
 
 }
